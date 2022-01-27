7e9b42e8db00d23fb4b6e74cd26f847c4b980b2c
talia bakircioglu <taliabakircioglu@talias-iMac.local>
2022-01-27 11:58:55 -0500

    Update recordset-thumbnails.php

--- datasource/recordset-thumbnails.php
+++ datasource/recordset-thumbnails.php
@@ -9,7 +9,7 @@ array (   // the first array inside "content":: child array #1
 		'id'                => "1",
     'metatitle'         => "Photo shoot at school",
     'metadescription'   => "Photography class and went to shoot photos",
-		'h1title'           => "Photo shoot at school",
+		'h1title'           => "Photo shoot at School",
 		'imagepath'         => "/img/thumbs/bakircioglu-talia-001-007.jpg",
 		'caption'           => "with the boys on the stairs",
     'copyright'         => "&copy Talia Bakircioglu, 2021"),  // last line ends with a ),
